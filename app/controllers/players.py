3299ba3a1a8213aa2cd3624ef12cc494ebda0e47
Jefferson Xavier <jeffexavier@gmail.com>
2026-01-30 23:37:43 -0300

    teste da rota de update player e adição do .venv/ no gitignore

--- app/controllers/players.py
+++ app/controllers/players.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import Session
 
 from app.database import SessionLocal
 from app.dependencies import get_db
-from app.schemas.player import PlayerCreate, PlayerResponse
+from app.schemas.player import PlayerCreate, PlayerUpdate, PlayerResponse
 from app.services import player_service
 
 router = APIRouter()
@@ -15,3 +15,7 @@ def create_player_route(player: PlayerCreate, db: Session = Depends(get_db)):
 @router.get("/{id}", response_model=PlayerResponse, status_code=status.HTTP_200_OK)
 def get_player_route(id, db: Session = Depends(get_db)):
     return player_service.get_player(db=db, player_id=id)
+
+@router.put("/{id}", response_model=PlayerResponse, status_code=status.HTTP_202_ACCEPTED)
+def update_player_route(id, player:PlayerUpdate, db: Session = Depends(get_db)):
+    return "teste"
