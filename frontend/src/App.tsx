c06fa10f28c3d9c665e555073f3236fcc11babb5
Jefferson Xavier <jeffexavier@gmail.com>
2026-02-06 21:55:53 -0300

    Criação da página de checkin e botão chamando requisição

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,17 @@
-import { Checkin } from './pages/Checkin'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Checkin from './pages/checkin/Checkin';
+import Match from './pages/match/Match';
 
 function App() {
   return (
-    <Checkin />
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Checkin />} />
+        <Route path='/matches' element={<Match />} />
+      </Routes>
+    </BrowserRouter>
+    
+  );
 }
 
-export default App
+export default App;
