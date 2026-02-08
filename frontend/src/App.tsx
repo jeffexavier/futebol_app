bdfe710f1b6d6384aefaacc785fa2099d6d81d72
Jefferson Xavier <jeffexavier@gmail.com>
2026-02-07 21:44:31 -0300

    criação da base do frontend com heroui-cli e vite

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,20 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Checkin from './pages/checkin/Checkin';
-import Match from './pages/match/Match';
+import { Route, Routes } from "react-router-dom";
+
+import IndexPage from "@/pages/index";
+import DocsPage from "@/pages/docs";
+import PricingPage from "@/pages/pricing";
+import BlogPage from "@/pages/blog";
+import AboutPage from "@/pages/about";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<Checkin />} />
-        <Route path='/matches' element={<Match />} />
-      </Routes>
-    </BrowserRouter>
-    
+    <Routes>
+      <Route element={<IndexPage />} path="/" />
+      <Route element={<DocsPage />} path="/docs" />
+      <Route element={<PricingPage />} path="/pricing" />
+      <Route element={<BlogPage />} path="/blog" />
+      <Route element={<AboutPage />} path="/about" />
+    </Routes>
   );
 }
 
--- frontend/src/services/checkinService.ts
+++
@@ -1,8 +0,0 @@
-import { api } from "./api";
-
-export const createCheckin = async (playerName: string) => {
-    const response = await api.post('/checkins/', {
-        name: playerName
-    })
-    return response.data;
-}
