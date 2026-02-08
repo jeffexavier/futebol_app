6548bc6baaee6b93aeb854fe0e649fd3ed53b8c5
Jefferson Xavier <jeffexavier@gmail.com>
2026-02-07 21:48:18 -0300

    adição de rota para checkin no front

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,11 +5,13 @@ import DocsPage from "@/pages/docs";
 import PricingPage from "@/pages/pricing";
 import BlogPage from "@/pages/blog";
 import AboutPage from "@/pages/about";
+import Checkin from "@/pages/checkin";
 
 function App() {
   return (
     <Routes>
-      <Route element={<IndexPage />} path="/" />
+      <Route element={<Checkin />} path="/"/>
+      <Route element={<IndexPage />} path="/index" />
       <Route element={<DocsPage />} path="/docs" />
       <Route element={<PricingPage />} path="/pricing" />
       <Route element={<BlogPage />} path="/blog" />
