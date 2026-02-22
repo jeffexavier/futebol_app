2e7056011487e2fd6edc82f698baac5870849d4f
Jefferson Xavier <jeffexavier@gmail.com>
2026-02-21 22:21:21 -0300

    ajuste no getMatch do pagina matches de user normal

--- frontend/src/pages/match.tsx
+++ frontend/src/pages/match.tsx
@@ -1,9 +1,27 @@
 import MatchesTables from "@/components/match/matchesTables";
+import { getMatch } from "@/services/match";
+import { MatchResponse } from "@/types/match";
+import { useEffect, useState } from "react";
 
 export default function Match() {
+
+    const [matchData, setMatchData] = useState<MatchResponse | null>(null)
+
+    async function handleGetCheckin() {
+        const matchTeamsList = await getMatch();
+        setMatchData(matchTeamsList)
+        console.log(matchTeamsList)
+    }
+
+    useEffect(() => {
+        if (matchData === null) {
+            handleGetCheckin();
+        }
+    })
+
     return (
         <div className="flex flex-col justify-top items-center min-h-screen w-screen p-4 gap-6">
-            <MatchesTables matchTeamsList={null} fromAdminPage={null}/>
+            <MatchesTables matchTeamsList={matchData} fromAdminPage={null}/>
         </div>
     );
 }
