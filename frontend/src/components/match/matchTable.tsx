1684b5c8a12dbd7429484b7f45a3a8e53db8f2d6
Jefferson Xavier <jeffexavier@gmail.com>
2026-03-05 22:07:46 -0300

    ajuste no front do updatecheckin para evitar keys repetidas

--- frontend/src/components/match/matchTable.tsx
+++ frontend/src/components/match/matchTable.tsx
@@ -46,6 +46,8 @@ export default function MatchTable({
     setCheckinIdToChange?.(checkinId);
   }
 
+  console.log(onMoveItem && teamData && teamData.length > 0);
+
   return (
     <Table aria-label="Table">
       <TableHeader>
@@ -59,9 +61,7 @@ export default function MatchTable({
       >
         <Fragment>
           { onMoveItem && teamData && teamData.length > 0 &&
-            <TableRow className="dark cursor-pointer h-12 bg-foreground-100" key={`${teamData[0].id}, emptySpace`} onClick={() => handleChooseNewPosition?.(teamData[0
-              
-            ].id, PositionChoice.Before, chosenTeam)}>
+            <TableRow className="dark cursor-pointer h-12 bg-foreground-100" key={`${teamData[0].id}, firstEmptySpace`} onClick={() => handleChooseNewPosition?.(teamData[0].id, PositionChoice.Before, chosenTeam)}>
               <TableCell>
                 {""}
               </TableCell>
